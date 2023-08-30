7be4b9909df0bb3ff5f0aa541f65317f937bf1a1
unknown <rrrr0ffff@gmail.com>
2023-08-30 18:31:34 -0300

    added fetch on mount

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import { type Image } from "./types/image";
+
 	let images: Image[] = [];
 	let query = "";
-	async function fetchImages(e) {
+
+	function handleSubmit(e: SubmitEvent) {
 		e.preventDefault();
+		fetchImages();
+	}
+
+	async function fetchImages() {
 		const response = await fetch(
-			`https://api.unsplash.com/search/photos?query=${query}&per_page=3&client_id=${
-				import.meta.env.VITE_API_KEY
-			}`
+			`https://api.unsplash.com/search/photos?query=${
+				query || "svelte"
+			}&per_page=6&client_id=${import.meta.env.VITE_API_KEY}`
 		);
 		const data = await response.json();
 		images = data.results;
 	}
+
+	onMount(() => fetchImages());
 </script>
 
 <main>
-	<form on:submit={fetchImages}>
+	<form on:submit={handleSubmit}>
 		<input type="text" bind:value={query} />
 		<button>SEARCH</button>
 	</form>
 	{#each images as image}
-		<img src={image.urls.regular} alt={image.alt_description} />
+		<img src={image.urls.small} alt={image.alt_description} />
 	{/each}
 </main>
