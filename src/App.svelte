2b1b6c6104484e55d916566641210245b694f943
unknown <rrrr0ffff@gmail.com>
2023-09-08 00:15:43 -0300

    styling

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
 	import { onMount } from "svelte";
 	import { type Image } from "./types/image";
 	import { fly } from "svelte/transition";
+	import Icon from "@iconify/svelte";
+	import "@fontsource/poppins";
 
 	let images: Image[] = [];
 	let query = "";
@@ -42,11 +44,25 @@
 	</form>
 	<div class="images">
 		{#each images as image, index (image.id)}
-			<img
-				src={image.urls.small}
-				alt={image.alt_description}
-				in:fly={{ y: 200, duration: 800, delay: index * 80 }}
-			/>
+			<div class="card" in:fly={{ y: 200, duration: 800, delay: index * 80 }}>
+				<img src={image.urls.small} alt={image.alt_description} />
+				<div class="user">
+					<a href={`https://unsplash.com/@${image.user.username}`}
+						>{image.user.first_name} {image.user.last_name}</a
+					>
+					<div class="social">
+						<a href={`https://twitter.com/${image.user.twitter_username}`}>
+							<Icon icon="ion:logo-twitter" width="25" />
+						</a>
+						<a href={`https://instagram.com/${image.user.instagram_username}`}
+							><Icon icon="ion:logo-instagram" width="25" /></a
+						>
+						<a href={`${image.user.portfolio_url}`}
+							><Icon icon="ion:globe-outline" width="25" /></a
+						>
+					</div>
+				</div>
+			</div>
 		{/each}
 	</div>
 	<button on:click={handleMore}>More...</button>
@@ -56,10 +72,11 @@
 	main {
 		padding: 2%;
 		align-items: center;
-		background-color: #1f1e25;
+		background-color: #070913;
 		min-height: 100dvh;
 		display: flex;
 		flex-direction: column;
+		font-family: "Poppins", sans-serif;
 		gap: 3vh;
 	}
 
@@ -91,11 +108,46 @@
 		gap: 10px;
 	}
 
+	.card {
+		align-items: center;
+		background-color: #ececec;
+		border-radius: 15px;
+		display: flex;
+		flex-direction: column;
+		overflow: hidden;
+	}
+
 	img {
 		aspect-ratio: 1/1;
-		border-radius: 15px;
 		width: 400px;
 		object-fit: cover;
+	}
+
+	.user {
+		align-items: center;
+		display: flex;
+		font-size: 1.1rem;
+		font-weight: bold;
+		justify-content: space-between;
+		padding: 10px;
+		width: 100%;
+	}
+
+	a {
+		text-decoration: none;
+		color: inherit;
+		display: grid;
+		place-content: center;
+
+		&:visited {
+			color: #313238;
+		}
+	}
+
+	.social {
+		display: flex;
+		gap: 5px;
+		justify-content: end;
 	}
 
 	@media (min-width: 780px) {
